06680149fe8afee9f0c5269408fe716020ee4cd0
MirkoBicchierai <bicchierai.mirko.developer@gmail.com>
2021-03-05 22:39:00 +0100

    add nothing

---
+++ src/DBConnection.java
@@ -0,0 +1,24 @@
+import java.sql.*;
+public class DBConnection {
+
+    private static Connection c = null;
+
+    public static Connection getInstance() {
+        if(c==null) {
+            try {
+                Class.forName("org.sqlite.JDBC");
+                c = DriverManager.getConnection("jdbc:sqlite:db/swe.db");
+                c.setAutoCommit(false);
+            } catch (Exception e ) {
+                System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+                System.exit(-1);
+            }
+        }
+        return c;
+    }
+
+    private DBConnection(){
+
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,39 +2,10 @@ import java.sql.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
+        Connection c = DBConnection.getInstance();
 
-        Connection c = null;
-        Statement stmt = null;
-        try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:db/swe.db");
-            c.setAutoCommit(false);
-            System.out.println("Opened database successfully");
-
-            stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery( "SELECT * FROM Article;" );
-
-            while ( rs.next() ) {
-                int id = rs.getInt("id");
-                String  name = rs.getString("name");
-                float price  = rs.getFloat("Price");
-
-
-                System.out.println( "ID = " + id );
-                System.out.println( "NAME = " + name );
-                System.out.println( "PRICE = " + price );
-                System.out.println();
-            }
-            rs.close();
-            stmt.close();
-            c.close();
-
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-            System.exit(0);
-        }
 
     }
 
