af42e8e57eeeec204e1b91b46ef9a2136911564a
MirkoBicchierai <bicchierai.mirko.developer@gmail.com>
2021-03-05 11:27:03 +0100

    SQlite

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
 
+import java.sql.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Dio cane");
+
+        Connection conn = null;
+        try {
+
+            conn = DriverManager.getConnection("jdbc:sqlite:db/swe.db");
+            System.out.println("Connection to SQLite has been established.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 
 }
